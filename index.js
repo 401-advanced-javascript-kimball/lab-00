2f0f0fe53a8570772a963e8cb152e21bb239755f
Jonathan A. Kimball <jonathan.kimball64@gmail.com>
2019-11-28 22:21:51 -0800

    setup

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 'use strict';
 
+// Application Dependencies
+const express = require('express');
+
+// Environment variables
+require('dotenv').config();
+
+// Application Setup
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Express middleware
+app.use(express.static('./docs'));
+
 const validator = require('./lib/validator.js');
 
+console.log('Testing...');
 console.log(validator.isValid('Test', validator.isString));
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
